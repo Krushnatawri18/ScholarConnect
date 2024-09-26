856bff2135e2a3a08599ad9d6abf6fd208736e07
Krushna Tawri <krishtawri123@gmail.com>
2024-09-26 19:57:31 +0530

    Add index file

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const connectWithDb = require('./config/Database');
+const cloudinaryConnect = require('./config/Cloudinary');
 require('dotenv').config();
+
+const profileRoutes = require('./routes/Profile');
+const fileRoutes = require('./routes/File');
+const userRoutes = require('./routes/User');
+const adminRoutes = require('./routes/Admin');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,6 +17,10 @@ app.use(fileUpload({
     useTempFiles: true, 
     tempFileDir: '/tmp/', 
 }));
+app.use('/api/v1/profile', profileRoutes);
+app.use('/api/v1/file', fileRoutes);
+app.use('/api/v1/user', userRoutes); 
+app.use('/api/v1/admin', adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`App started at port ${PORT}`);
@@ -20,4 +30,5 @@ app.get('/', (req, res)=> {
     res.send('Home Page');
 });
 
+cloudinaryConnect();
 connectWithDb();
