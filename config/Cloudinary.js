595975ad399b2b7fefeefe0d14a7a02005b28893
Krushna Tawri <krishtawri123@gmail.com>
2024-09-30 12:10:00 +0530

    Add models and config for database and cloudinary

--- config/Cloudinary.js
+++ config/Cloudinary.js
@@ -1,13 +1,14 @@
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
-const cloudinaryConnect = () => {
+exports.cloudinaryConnect = () => {
     try {
         cloudinary.config({
             cloud_name: process.env.CLOUD_NAME,
             api_key: process.env.API_KEY,
             api_secret: process.env.API_SECRET
         });
+        console.log('Cloudinary connected successfully');
     }
     catch (error) {
         console.error(error);
