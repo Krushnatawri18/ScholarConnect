81764f79eaafeb6e09f2d98f966e091d5a0478cb
Krushna Tawri <krishtawri123@gmail.com>
2024-09-30 12:12:15 +0530

    Add index file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const connectWithDb = require('./config/Database');
-const cloudinaryConnect = require('./config/Cloudinary');
+const { connectWithDb } = require('./config/Database');
+const { cloudinaryConnect } = require('./config/Cloudinary');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const cors = require('cors');
 
 const profileRoutes = require('./routes/Profile');
 const fileRoutes = require('./routes/File');
@@ -12,21 +14,29 @@ const adminRoutes = require('./routes/Admin');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(fileUpload({
-    useTempFiles: true, 
-    tempFileDir: '/tmp/', 
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
 }));
-app.use('/api/v1/profile', profileRoutes);
-app.use('/api/v1/file', fileRoutes);
-app.use('/api/v1/user', userRoutes); 
-app.use('/api/v1/admin', adminRoutes);
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true
+    })
+)
+
+app.use('/api/v1/profile/', profileRoutes);
+app.use('/api/v1/file/', fileRoutes);
+app.use('/api/v1/user/', userRoutes);
+app.use('/api/v1/admin/', adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`App started at port ${PORT}`);
 });
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('Home Page');
 });
 
